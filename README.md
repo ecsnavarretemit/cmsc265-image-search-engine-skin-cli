0aa76648727808731482a28f41c4508ac071e555
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-05-14 07:52:35 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CMSC 265 Exercise 8 - Image Search Engine (Skin)
+
+## License
+
+MIT
+
+
---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# run.py
+#
+# Author(s):
+#   Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+#
+# Licensed under MIT
+# Version 1.0.0-alpha1
+
+
